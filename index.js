4edb95a7dc3b11cd15ed51198603aa500d11c75e
Samuel Itoje <106639109+Samdoez@users.noreply.github.com>
2025-06-04 06:48:37 +0100

    Basic_ServerVerification_Project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+//Learn to always put next() when you build your custom middleware and any middle ware 
 
 import express from "express"; //all packages
 import { dirname } from "path"; // to get the absolute url when it been hosted on live server, so hat it would display correctly
